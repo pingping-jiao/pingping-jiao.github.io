5eb647206994ed12b8fed0f681e046a384e84bf0
pingping-jiao <77112416+pingping-jiao@users.noreply.github.com>
2021-01-07 10:33:38 -0500

    Update TicTacToe.js
    added cookie

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,4 +1,13 @@
 //Let's explore some events samples!
+//add cookie function
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+setCookie("foo", "a-b-c", 3);
 
 var cells = document.getElementsByTagName("td");
 // var cellOne = document.querySelectorAll("td")[0];
