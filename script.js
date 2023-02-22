552178ffa9bd3654021127e1ca4b973e6c329d22
pingping-jiao <77112416+pingping-jiao@users.noreply.github.com>
2023-02-22 10:56:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function xhrLoad1() {
   var xhttp = new XMLHttpRequest();
-
+   test
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       // Typical action to be performed when the document is ready:
