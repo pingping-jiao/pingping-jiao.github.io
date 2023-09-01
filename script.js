f38529b27909acbf1f6b8244079fde3f00b142a6
pingping-jiao <77112416+pingping-jiao@users.noreply.github.com>
2023-09-01 09:35:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function xhrLoad2() {
   xhttp.open("GET", "css.html", true);
   xhttp.send();
 }
+function makeEmail() {
+    var strValues = "abcdefg12345";
+    var strEmail = "";
+    var strTmp;
+    for (var i = 0; i < 10; i++) {
+        strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
+        strEmail = strEmail + strTmp;
+    }
+    strTmp = "";
+    strEmail = strEmail + "@";
+    for (var j = 0; j < 8; j++) {
+        strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
+        strEmail = strEmail + strTmp;
+    }
+    strEmail = strEmail + ".com"
+    return strEmail;
+}
